4f03e6f35dcf25f11d58f4ec8f7d5fcf1cac1cc4
austin <villasanamallariaustin@gmail.com>
2023-08-21 17:45:12 +0800

    back: create endpoints for signin and getting territories
    each endpoint is consuming external API provided by netzwelt

--- netzwelt-front-expressJS/src/index.js
+++ netzwelt-front-expressJS/src/index.js
@@ -1,11 +1,28 @@
+const { default: axios } = require('axios');
 const express = require('express');
 
 const app = express();
 
 const PORT = 8080
 
-app.get('/', (req, res) => {
-    res.send("HELLO WORLD");
+app.use(express.json());
+
+app.post('/signIn/', async (req, res) => {
+    try {
+        const resp = await axios.post('https://netzwelt-devtest.azurewebsites.net/Account/SignIn/', req.body);    
+        return res.send(resp.data);
+    } catch (error) {
+        return res.status(401).json(error.response.data);
+    }
+});
+
+app.get('/territories/', async (req, res, next) => {
+    try {
+        const resp = await axios.get('https://netzwelt-devtest.azurewebsites.net/Territories/All/');
+        return res.json(resp.data.data);
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.listen(PORT, () => {
