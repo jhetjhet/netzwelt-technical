f32a732f60c22afed07c99f3499d1ed2acb29bbd
austin <villasanamallariaustin@gmail.com>
2023-08-21 16:26:02 +0800

    front: make loginPage inputs controlled

--- netzwelt-front-react/src/pages/loginPage/index.jsx
+++ netzwelt-front-react/src/pages/loginPage/index.jsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
 import "./loginPage.css";
 
 const LoginPage = () => {
+    const DEFAULT_AUTH_DATA = {
+        username: "",
+        password: "",
+    }
+
+    const [authData, setAuthData] = useState({...DEFAULT_AUTH_DATA});
+
+    const __on_submit__ = (e) => {
+        e.preventDefault();
+        console.log(authData);
+    }
+
+    const __on_change__ = (e) => {
+        const { name, value } = e.target;
+        
+        let newAuthData = {...authData};
+        newAuthData[name] = value;
+        setAuthData(newAuthData);
+    }
 
     return (
         <div id="login-page">
-            <form>
+            <form onSubmit={__on_submit__}>
                 <label htmlFor="username-input">Username</label>
                 <input
                     id="username-input" 
                     type="text" 
                     placeholder="Username"
+                    name="username"
+                    onChange={__on_change__}
                 />
                 <label htmlFor="password-input">Password</label>
                 <input 
                     id="password-input"
                     type="password" 
                     placeholder="Password"
+                    name="password"
+                    onChange={__on_change__}
                 />
                 <button>login</button>
             </form>
