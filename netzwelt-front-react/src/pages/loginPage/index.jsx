de07be6f0e5c6e0105287ca0a1435ffc449a3180
austin <villasanamallariaustin@gmail.com>
2023-08-21 22:28:36 +0800

    front: make loginPage redirect to other route when already logged in

--- netzwelt-front-react/src/pages/loginPage/index.jsx
+++ netzwelt-front-react/src/pages/loginPage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./loginPage.css";
 import { useAuthenticationContext } from "../../components/AuthenticationProvider";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -9,23 +9,29 @@ const LoginPage = () => {
         password: "",
     }
 
-    const [authData, setAuthData] = useState({...DEFAULT_AUTH_DATA});
-    
-    const { login } = useAuthenticationContext() ;
+    const [authData, setAuthData] = useState({ ...DEFAULT_AUTH_DATA });
+
+    const { login, isAuthenticated } = useAuthenticationContext();
 
     const location = useLocation();
 
     const navigate = useNavigate();
 
+    let from = "/home/index/";
+    if (location.state && location.state.from) { // check if there is a state of 'from'
+        from = location.state.from;
+    }
+
+    useEffect(() => {
+        if (isAuthenticated()) {
+            navigate(from, { replace: true });
+        }
+    });
+
     const __on_submit__ = (e) => {
         e.preventDefault();
         login(authData.username, authData.password, (data, err) => {
-            if(!err) {
-                let from = "/";
-                if(location.state && location.state.from) { // check if there is a state of 'from'
-                    from = location.state.from;
-                }
-
+            if (!err) {
                 // redirect to homepage or to previous link
                 navigate(from, { replace: true });
             }
@@ -34,8 +40,8 @@ const LoginPage = () => {
 
     const __on_change__ = (e) => {
         const { name, value } = e.target;
-        
-        let newAuthData = {...authData};
+
+        let newAuthData = { ...authData };
         newAuthData[name] = value;
         setAuthData(newAuthData);
     }
@@ -45,16 +51,16 @@ const LoginPage = () => {
             <form onSubmit={__on_submit__}>
                 <label htmlFor="username-input">Username</label>
                 <input
-                    id="username-input" 
-                    type="text" 
+                    id="username-input"
+                    type="text"
                     placeholder="Username"
                     name="username"
                     onChange={__on_change__}
                 />
                 <label htmlFor="password-input">Password</label>
-                <input 
+                <input
                     id="password-input"
-                    type="password" 
+                    type="password"
                     placeholder="Password"
                     name="password"
                     onChange={__on_change__}
