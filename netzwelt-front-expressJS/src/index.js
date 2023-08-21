77517ab779f180539f903a23f735625052252ad1
austin <villasanamallariaustin@gmail.com>
2023-08-21 17:18:53 +0800

    back: initial express setup

--- netzwelt-front-expressJS/src/index.js
+++ netzwelt-front-expressJS/src/index.js
@@ -1,1 +1,13 @@
-console.log("HELLO WORLD");
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080
+
+app.get('/', (req, res) => {
+    res.send("HELLO WORLD");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at port:${PORT}`);
+});
